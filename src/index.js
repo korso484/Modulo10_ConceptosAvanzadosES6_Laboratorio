b69b55c1c8aadd672cec4010a2df0635ffae8026
korso484 <korso382@gmail.com>
2020-08-07 13:57:01 +0200

    Required OK

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-const hasId = (object) => {
-    const { id } = object ? true : false;
-}
+//Data
 
 const user = {
     name: "Dani",
@@ -8,5 +6,68 @@ const user = {
     age: 37
 }
 
+const fruits = ["apple", "orange", "banana"];
+
+const numbers = [1, 2, 3, 4];
+
+/////////////////////////////hasId/////////////////////////////
+
+const hasId = ({ id }) => id ? true : false;
+
 console.log(hasId(user));
+
+/////////////////////////////head/////////////////////////////
+
+const head = ([first]) => first;
+
+console.log(head(fruits));
+
+/////////////////////////////tail/////////////////////////////
+
+const tail = ([, ...restItems]) => [restItems];
+
+console.log(tail(fruits));
+
+/////////////////////////////swapFirstToLast/////////////////////////////
+
+const swapFirstToLast = ([first, ...restItems]) => [restItems.concat(first)];
+
+console.log(swapFirstToLast(fruits));
+
+/////////////////////////////excludeId/////////////////////////////
+
+const excludeId = ({ id, ...restObject }) => restObject;
+
+console.log(excludeId(user));
+
+/////////////////////////////wordsStartingWithA/////////////////////////////
+
+const wordsStartingWithA = arr => arr.filter(item => item.startsWith('a'));
+
+console.log(wordsStartingWithA(fruits));
+
+/////////////////////////////concat/////////////////////////////
+
+const concat = (arr) => arr.join(" | ");
+
+console.log(concat(fruits));
+
+/////////////////////////////multArray/////////////////////////////
+
+const multArray = (arr, x) => arr.map(item => {
+    return item * x;
+});
+
+console.log(multArray(numbers, 2));
+
+/////////////////////////////calcMult/////////////////////////////
+
+const calcMult = (myArgs) => myArgs.reduce((acc, arg) => acc * arg, 1);
+
+console.log(calcMult(numbers));
+
+//////////////////////////////////////////////////////////EXTRA
+
+
+
 

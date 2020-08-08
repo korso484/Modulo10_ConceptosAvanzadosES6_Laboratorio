9fc9c5142ec0d62bd4d705af3c202f00e289b991
korso484 <korso382@gmail.com>
2020-08-08 19:55:26 +0200

    Everything Done

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const user = {
 
 const fruits = ["apple", "orange", "banana"];
 
+const names = ["Peter", "Pamela", "Percival"];
+
+const vehicles = ["car", "motorcycle", "plane", "boat"];
+
 const numbers = [1, 2, 3, 4];
 
 /////////////////////////////hasId/////////////////////////////
@@ -66,7 +70,100 @@ const calcMult = (myArgs) => myArgs.reduce((acc, arg) => acc * arg, 1);
 
 console.log(calcMult(numbers));
 
-//////////////////////////////////////////////////////////EXTRA
+//EXTRA
+
+/////////////////////////////swapFirstSecond/////////////////////////////
+
+const swapFirstSecond = ([first, second, ...restItems]) => [second, first, ...restItems];
+
+console.log(swapFirstSecond(fruits));
+
+/////////////////////////////firstEqual/////////////////////////////
+
+const firstEqual = (myArgs, char) => myArgs.every(arg => arg.startsWith(char));
+
+console.log(firstEqual(names, 'P'));
+
+/////////////////////////////longest/////////////////////////////
+
+const longest = (arr) => arr.reduce((a, b) => { return a.length > b.length ? a : b });
+
+console.log(longest(vehicles));
+
+/////////////////////////////searchInStringV1/////////////////////////////
+
+const searchInStringV1 = (str, char) =>
+    Array.from(str).reduce((acc, item) => {
+        if (item === char) acc++;
+        return acc;
+    }, 0);
+
+console.log(searchInStringV1("calabaza", 'a'));
+
+/////////////////////////////searchInStringV2/////////////////////////////
+
+const searchInStringV2 = (str, char) =>
+    Array.from(str).map(item => {
+        if (item === char) return 1;
+    });
+
+console.log(searchInStringV1("hola", 'a'));
+
+/////////////////////////////sortCharacters/////////////////////////////
+
+const sortCharacters = (str) => Array.from(str).sort();
+
+console.log(sortCharacters("murcielago"));
+
+/////////////////////////////shout/////////////////////////////
+
+const shout = (myArgs) =>
+    myArgs.map(item => item.toUpperCase())
+        .reduce((acc, itemUpper) => acc + " ¡ " + itemUpper + "! ", "");
+
+console.log(shout(names));
+
+//Lista de la compra
+
+const shoppingCart = [
+    { category: "Frutas y Verduras", product: "Lechuga", price: 0.8, units: 1 },
+    { category: "Carne y Pescado", product: "Pechuga pollo", price: 3.75, units: 2 },
+    { category: "Droguería", product: "Gel ducha", price: 1.15, units: 1 },
+    { category: "Droguería", product: "Papel cocina", price: 0.9, units: 3 },
+    { category: "Frutas y Verduras", product: "Sandía", price: 4.65, units: 1 },
+    { category: "Frutas y Verduras", product: "Puerro", price: 4.65, units: 2 },
+    { category: "Carne y Pescado", product: "Secreto ibérico", price: 5.75, units: 2 },
+];
+
+//Obtén una nueva lista donde aparezca el IVA (21%) de cada producto.
+
+const addIVA = (tax, products) => products
+    .map(product => ({ ...product, IVA: (tax / 100 * product.price).toFixed(2) }));
+
+console.log(addIVA(21, shoppingCart));
+
+//Ordena la lista de más a menos unidades.
+
+const orderMax = (products) => products.sort((p1, p2) => p2.units - p1.units);
+
+console.log(orderMax(shoppingCart));
+
+//Obtén el subtotal gastado en droguería.
+
+const subTotalD = products => products
+    .filter(product => product.category === 'Droguería')
+    .reduce((acc, product) => acc + (product.price * product.units), 0);
+
+console.log(subTotalD(shoppingCart));
+
+//Obtén la lista por consola en formato "Producto -> Precio Total €" y ordenada por categoría.
+
+const newOrderList = products => products
+    .sort((p1, p2) => (p1.category < p2.category) ? -1 : 1)
+    .map(product => ({ name: product.product, price: product.units * product.price }))
+    .reduce((acc, product) => acc + "Producto: " + product.name + " -> Precio total " + product.price + " € \n", "");
+
+console.log(newOrderList(shoppingCart));
 
 
 
